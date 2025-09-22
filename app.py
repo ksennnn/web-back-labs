8338c2bbb5fba0da484b09b998aa20723f5a16ef
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-09-22 20:54:21 +0700

    Стили

--- app.py
+++ app.py
@@ -38,9 +38,13 @@ def author():
 @app.route("/image")
 def image():
     path = url_for("static", filename="oak.jpg")
+    style_path = url_for("static", filename="lab1.css")
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="''' + style_path + '''">
+    </head>
     <body>
         <h1>Дуб</h1>
         <img src="''' + path + '''">
