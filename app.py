25f1a1d0030501c5ee1d59c628c0445bc35e4069
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-09-24 18:07:37 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -440,4 +440,7 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Ковалёва Ксения'
-    return render_template('example.html', name=name)
+    number = '2'
+    group = 'ФБИ-32'
+    kurs = '3'
+    return render_template('example.html', name=name, number=number, group=group, kurs=kurs)
