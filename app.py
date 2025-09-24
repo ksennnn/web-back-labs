b0133ba7522e269cde8aa0fb7c9a5a835c96b02d
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-09-24 18:03:11 +0700

    Имя студента

--- app.py
+++ app.py
@@ -439,5 +439,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
-    
+    name = 'Ковалёва Ксения'
+    return render_template('example.html', name=name)
