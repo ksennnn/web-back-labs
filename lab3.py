bc303ebaf3455c15eaeb6fdc00c7e469bcc21b06
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-10-21 23:06:39 +0700

    Очистка кук

--- lab3.py
+++ lab3.py
@@ -140,3 +140,13 @@ def ticket():
     return render_template('/lab3/train_ticket.html', fio=fio, polka=polka, belie=belie, bagazh=bagazh, age=age_int, 
                         viezd=viezd, naznachenie=naznachenie, data=data, strahovka=strahovka, base_price=base_price,
                         polka_price=polka_price, total_price=total_price)
+
+
+@lab3.route("/lab3/settings/reset")
+def reset_settings():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('font_family')
+    return resp
