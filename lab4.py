aede8df5c008e3ccc6b90e10b059f35b677e5bf5
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-11-04 15:01:40 +0700

    Деление доделано

--- lab4.py
+++ lab4.py
@@ -20,5 +20,7 @@ def div():
         return render_template('lab4/div.html', error='Оба поля должны быть заполнены!')
     x1 = int(x1)
     x2 = int(x2)
+    if x2 == 0:
+        return render_template('lab4/div.html', error='На ноль делить нельзя!')
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
