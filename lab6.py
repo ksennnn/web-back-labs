d4c181a316b382c44ee1c69258e3cf94af84ec6c
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-12-02 19:00:31 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -52,6 +52,34 @@ def api():
                     'result': 'success',
                     'id': id
                 }
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 2,
+                            'message': 'Not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Cannot cancel someone else booking'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
     return {
             'jsonrpc': '2.0',
             'error': {
