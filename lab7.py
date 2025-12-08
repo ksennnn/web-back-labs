bf667014df90a04c6b02c91bc3956289dfafb1e3
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-12-08 16:31:37 +0700

    Удаление выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -53,3 +53,11 @@ def get_films(id):
     if id < 0 or id >= len(films):
         abort(404) 
     return films[id]
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
+def del_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+    del films[id]
+    return '', 204
