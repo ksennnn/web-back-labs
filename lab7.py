efa3e761fc8f3e47eaba873ac23a081c48fd096d
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-12-08 20:02:31 +0700

    Доп. задание

--- lab7.py
+++ lab7.py
@@ -1,68 +1,114 @@
-from flask import Blueprint, render_template, request, abort, redirect, session, current_app
+from flask import Blueprint, render_template, request, abort, current_app
+import psycopg2
+from psycopg2.extras import RealDictCursor
 import sqlite3
 from os import path
+from datetime import datetime
+
 
 lab7 = Blueprint('lab7', __name__)
+
+
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+                host = '127.0.0.1',
+                database = 'ksenia_kovalyova_knowledge_base',
+                user = 'ksenia_kovalyova_knowledge_base',
+                password = '140981'
+            )
+        cur = conn.cursor(cursor_factory= RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 @lab7.route('/lab7/')
 def main():
     return render_template('lab7/index.html')
 
 
-films = [
-    {
-        "title": "The Matrix",
-        "title_ru": "Матрица",
-        "year": 1999,
-        "description": "Нео узнаёт, что мир, в котором живёт человечество, — всего лишь симуляция, \
-        созданная машинами. Он вступает в сопротивление, чтобы освободить людей от цифрового рабства."
-    },
-    {
-        "title": "Gladiator",
-        "title_ru": "Гладиатор",
-        "year": 2000,
-        "description": "Римский генерал Максимус становится жертвой предательства и попадает в рабство. \
-        Судьба делает его гладиатором, который должен бороться не только за жизнь, но и за справедливость."
-    },
-    {
-        "title": "The Lord of the Rings: The Fellowship of the Ring",
-        "title_ru": "Властелин колец: Братство кольца",
-        "year": 2001,
-        "description": "Фродо отправляется в опасное путешествие, чтобы уничтожить Кольцо Всевластия, \
-        способное поработить весь мир Средиземья."
-    },
-    {
-        "title": "Avatar",
-        "title_ru": "Аватар",
-        "year": 2009,
-        "description": "Бывший морской пехотинец Джейк Салли оказывается на Пандоре, где должен выбрать \
-        сторону: служить корпорации или защитить народ На'ви и их уникальный мир."
-    },
-    {
-        "title": "The Dark Knight",
-        "title_ru": "Тёмный рыцарь",
-        "year": 2008,
-        "description": "Бэтмен сталкивается с Джокером — преступным гением, бросающим вызов всем законам. \
-        Ставки высоки: судьба Готэма и моральные границы самого героя."
-    }
-]
-
-
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
+    conn, cur = db_connect()
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT id, title, title_ru, year, description FROM films ORDER BY id")
+    else:
+        cur.execute("SELECT id, title, title_ru, year, description FROM films ORDER BY id")
+    
+    rows = cur.fetchall()
+    db_close(conn, cur)
+    
+    films = []
+    for row in rows:
+        films.append({
+            "id": row["id"],
+            "title": row["title"],
+            "title_ru": row["title_ru"],
+            "year": row["year"],
+            "description": row["description"]
+        })
+    
     return films
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
 def get_film(id):
-    if id < 0 or id >=len(films):
+    conn, cur = db_connect()
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT id, title, title_ru, year, description FROM films WHERE id = %s", (id,))
+    else:
+        cur.execute("SELECT id, title, title_ru, year, description FROM films WHERE id = ?", (id,))
+    
+    row = cur.fetchone()
+    db_close(conn, cur)
+
+    if row is None:
         abort(404)
-    return films[id]
+
+    film = {
+        "id": row["id"],
+        "title": row["title"],
+        "title_ru": row["title_ru"],
+        "year": row["year"],
+        "description": row["description"]
+    }
+
+    return film  
+
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
 def del_film(id):
-    if id < 0 or id >= len(films):
-        abort(404)
-    del films[id]
+    conn, cur = db_connect()
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT id FROM films WHERE id = %s", (id,))
+    else:
+        cur.execute("SELECT id FROM films WHERE id = ?", (id,))
+    
+    exists = cur.fetchone()
+    
+    if not exists:
+        db_close(conn, cur)
+        abort(404, description=f"Фильм с ID {id} не найден")
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("DELETE FROM films WHERE id = %s", (id,))
+    else:
+        cur.execute("DELETE FROM films WHERE id = ?", (id,))
+    
+    db_close(conn, cur)
+
     return '', 204
 
 
@@ -73,12 +119,53 @@ def put_film(id):
     film = request.get_json()
     if film['description'] == '':
         return {'description': 'Заполните описание'}, 400
+    if len(film['description']) > 2000:
+        return {'description': 'Описание не должно превышать 2000 символов'}, 400
     
     if film['title'] == '':
         film['title'] = film['title_ru']
     
-    films[id] = film
-    return films[id]
+    if film['title'] == '' and film['title_ru'] == '':
+        return {'title': 'Заполните название'}, 400
+
+    if film['title_ru'] == '':
+        return {'title_ru': 'Заполните название'}, 400
+
+    current_year = datetime.now().year
+
+    if 'year' not in film or film['year'].strip() == '' or not film['year'].isdigit():
+        return {'year': 'Год должен быть числом'}, 400
+
+    year = int(film['year'])
+    if year < 1895 or year > current_year:
+        return {'year': f'Введите год от 1895 до {current_year}'}, 400
+    
+    conn, cur = db_connect()
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("UPDATE films SET title=%s, title_ru=%s, year=%s, description=%s WHERE id=%s RETURNING *;", 
+                    (film['title'], film['title_ru'], film['year'], film['description'], id))
+        updated = cur.fetchone()
+        conn.commit()
+    else:
+        cur.execute("UPDATE films SET title=?, title_ru=?, year=?, description=? WHERE id=?;", 
+                    (film['title'], film['title_ru'], film['year'], film['description'], id))
+        conn.commit()
+
+        cur.execute("SELECT * FROM films WHERE id=?;", (id,))
+        updated = cur.fetchone()
+
+    if updated is None:
+        abort(404)
+    
+    db_close(conn, cur)
+
+    return {
+        "id": updated["id"],
+        "title": updated["title"],
+        "title_ru": updated["title_ru"],
+        "year": updated["year"],
+        "description": updated["description"]
+    }
 
 
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
@@ -86,9 +173,42 @@ def add_film():
     film = request.get_json()
     if film['description'] == '':
         return {'description': 'Заполните описание'}, 400
+    if len(film['description']) > 2000:
+        return {'description': 'Описание не должно превышать 2000 символов'}, 400
     
     if film['title'] == '':
         film['title'] = film['title_ru']
 
-    films.append(film)
-    return {'id': len(films) - 1}, 201
+    if film['title'] == '' and film['title_ru'] == '':
+        return {'title': 'Заполните название'}, 400
+
+    if film['title_ru'] == '':
+        return {'title_ru': 'Заполните название'}, 400
+    
+    current_year = datetime.now().year
+
+    if 'year' not in film or film['year'].strip() == '' or not film['year'].isdigit():
+        return {'year': 'Год должен быть числом'}, 400
+
+    year = int(film['year'])
+    if year < 1895 or year > current_year:
+        return {'year': f'Введите год от 1895 до {current_year}'}, 400
+        
+
+    conn, cur = db_connect()
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("INSERT INTO films (title, title_ru, year, description) VALUES (%s, %s, %s, %s) RETURNING id;", 
+                    (film['title'], film['title_ru'], film['year'], film['description']))
+        new_id = cur.fetchone()['id']
+        conn.commit()
+    else:
+        cur.execute("INSERT INTO films (title, title_ru, year, description) VALUES (?, ?, ?, ?);", 
+                    (film['title'], film['title_ru'], film['year'], film['description']))
+
+        conn.commit()
+        new_id = cur.lastrowid
+
+    db_close(conn, cur)
+
+    return {'id': new_id}, 201
--- static/lab7/main.js
+++ static/lab7/main.js
@@ -21,13 +21,13 @@ function fillFilmList() {
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
              editButton.onclick = function() {
-                editFilm(i);
+                 editFilm(films[i].id);
             }
             
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
             delButton.onclick = function() {
-                deleteFilm(i, films[i].title_ru);
+                deleteFilm(films[i].id, films[i].title_ru);
             };
 
             tdActions.append(editButton);
@@ -54,6 +54,9 @@ function deleteFilm(id, title) {
 }
 
 function showModal() {
+    document.getElementById('title-error').innerText = '';
+    document.getElementById('year-error').innerText = '';
+    document.getElementById('title-ru-error').innerText = '';
     document.getElementById('description-error').innerText = '';
     document.querySelector('div.modal').style.display = 'block';
 }
@@ -73,6 +76,9 @@ function addFilm() {
     document.getElementById('year').value = '';
     document.getElementById('description').value = '';
     document.getElementById('description-error').innerText = '';
+    document.getElementById('title-error').innerText = '';
+    document.getElementById('year-error').innerText = '';
+    document.getElementById('title-ru-error').innerText = '';
     showModal();
 }
 
@@ -102,11 +108,14 @@ function sendFilm() {
         return resp.json();
     })
     .then(function(errors) {
-        if(errors.description) {
+        if(errors.description)
             document.getElementById('description-error').innerText = errors.description;
-        } else { 
-            document.getElementById('description-error').innerText = '';
-        }
+        if(errors.year)
+            document.getElementById('year-error').innerText = errors.year;
+        if(errors.title)
+            document.getElementById('title-error').innerText = errors.title;
+        if(errors.title_ru)
+            document.getElementById('title-ru-error').innerText = errors.title_ru;
     });
 }
 
@@ -121,6 +130,9 @@ function editFilm(id) {
         document.getElementById('title-ru').value = film.title_ru;
         document.getElementById('year').value = film.year;
         document.getElementById('description').value = film.description;
+        document.getElementById('title-error').innerText = '';
+        document.getElementById('title-ru-error').innerText = '';
+        document.getElementById('year-error').innerText = '';
         document.getElementById('description-error').innerText = '';
         showModal();
     });
