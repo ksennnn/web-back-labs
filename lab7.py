53cc6221c56882a74c3f4e8e2381d1c43dae8bf6
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-12-08 20:12:19 +0700

    Правки допа

--- lab7.py
+++ lab7.py
@@ -114,8 +114,6 @@ def del_film(id):
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
 def put_film(id):
-    if id < 0 or id >= len(films):
-        abort(404)
     film = request.get_json()
     if film['description'] == '':
         return {'description': 'Заполните описание'}, 400
