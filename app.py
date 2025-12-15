97d03ef2201bd0c3b6e82ad339c121793f946e8e
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-12-15 23:47:32 +0700

    Правки

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ login_manager.init_app(app)
 def load_user(login_id):
     return users.query.get(int(login_id))
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'my-super-secret-key-123')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно секретный-секрет').encode('utf-8')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
 
