bcf0021cbe4b9772631147a2370546b15f9933a5
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-12-15 19:43:07 +0700

    Заготовка к 8-ой лабораторной

---
+++ lab8.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, abort, current_app
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import sqlite3
+from os import path
+
+
+lab8 = Blueprint('lab8', __name__)
+
+
+@lab8.route('/lab8/')
+def lab():
+    return render_template('lab8/lab8.html')
+
+
+@lab8.route('/lab8/register')
+def register():
+    return render_template('lab8/register.html')
+
+
+@lab8.route('/lab8/login')
+def login():
+    return render_template('lab8/login.html')
+
+
+@lab8.route('/lab8/articles')
+def articles():
+    return render_template('lab8/articles.html')
+
+
+@lab8.route('/lab8/create')
+def create():
+    return render_template('lab8/create.html')
