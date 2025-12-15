ab6df624366961faae9d82ecd8e2ca95d5bc502c
Ковалёва Ксения <kovalyovaks28@mail.ru>
2025-12-15 20:40:47 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, abort, current_app
-
+from flask import Blueprint, render_template, redirect, request, abort
+from db import db
+from db.models import users, articles
 from os import path
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 lab8 = Blueprint('lab8', __name__)
@@ -11,9 +13,29 @@ def lab():
     return render_template('lab8/lab8.html')
 
 
-@lab8.route('/lab8/register')
+@lab8.route('/lab8/register', methods=['GET', 'POST'])
 def register():
-    return render_template('lab8/register.html')
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+    
+    if not login_form or not login_form.strip():
+        return render_template('lab8/register.html', error='Введите логин')
+    
+    if not password_form or not password_form.strip():
+        return render_template('lab8/register.html', error='Введите пароль')
+
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
 
 
 @lab8.route('/lab8/login')
